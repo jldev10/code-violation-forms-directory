816064acd02e2e7c46683cf24de715dd128d1788
ty.mould <ty.mould@outlook.com>
2026-03-04 01:57:12 +0000

    File changes

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -69,6 +69,7 @@ export default function Header({ notificationBell }) {
                 {link.label}
               </a>
             ))}
+            
             {notificationBell}
           </nav>
           
